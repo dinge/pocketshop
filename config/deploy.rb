aecb56b21522575aba1d5ba6c15b1c5cc6c8fe67
Lars Gollnow <lg@mfind.de>
2009-02-19 21:16:57 +0100

    * added deploy_to

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,11 +9,7 @@ set :deploy_via, :copy
 set :copy_strategy, :export
 set :branch, "with_neo4j"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-
+set :deploy_to, "/home/railshost/apps/#{application}"
 
 role :app, "roboterliebe.de"
 role :web, "roboterliebe.de"
