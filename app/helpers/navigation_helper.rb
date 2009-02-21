04088d71c32c13bd61ce2cdea74963ba8e61c39e
Lars Gollnow <lg@mfind.de>
2009-02-21 18:07:27 +0100

    * small fix

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -72,7 +72,7 @@ module NavigationHelper
     control_list_container :container => :div, :id => :footer_navigation, :class => :navigation do
       [
        "me: #{link_to(Me.now.name, me_index_path)}",
-        "last action: #{ link_to(Me.now.last_action, Me.now.last_action) }",
+        # "last action: #{ link_to(Me.now.last_action, Me.now.last_action) }",
         # "last action at: #{ Me.now.last_action_at.to_formatted_s(:db) }",
         link_to('logout', logout_path, :method => :delete)
       ]
