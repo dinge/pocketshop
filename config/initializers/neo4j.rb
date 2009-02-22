59679c772cb02268e10e539072b59f7165fd9bcc
Lars Gollnow <lg@mfind.de>
2009-02-22 21:14:02 +0100

    * small fix

--- config/initializers/neo4j.rb
+++ config/initializers/neo4j.rb
@@ -12,9 +12,11 @@ elsif Rails.env.production?
   end
 end
 
-Lucene::Config[:store_on_file] = true
-Lucene::Config[:storage_path] = File.join(neo4j_base_path, 'lucene')
-Neo4j::Config[:storage_path] = File.join(neo4j_base_path, 'neo4j')
+if [Rails.env.testing?, Rails.env.develepment?].any?
+  Lucene::Config[:store_on_file] = true
+  Lucene::Config[:storage_path] = File.join(neo4j_base_path, 'lucene')
+  Neo4j::Config[:storage_path] = File.join(neo4j_base_path, 'neo4j')
 
-puts "storing neo4j's lucene index in #{Lucene::Config[:storage_path]}"
-puts "storing neo4j's database in #{Neo4j::Config[:storage_path]}"
+  puts "storing neo4j's lucene index in #{Lucene::Config[:storage_path]}"
+  puts "storing neo4j's database in #{Neo4j::Config[:storage_path]}"
+end
