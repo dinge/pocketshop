dae08c51eed8d8b198f24a19da7479a34e712178
Lars Gollnow <lg@mfind.de>
2009-02-24 23:05:15 +0100

    * added basic DingDealer::Guid

---
+++ lib/ding_dealer/guid.rb
@@ -0,0 +1,121 @@
+module DingDealer
+  class Guid
+
+    cattr_accessor :version
+    attr_reader :version
+
+    @@version = 2
+
+    class InvalidVersionFormat < StandardError; end
+    class InvalidCoderVersion < StandardError; end
+
+
+    def initialize(version = @@version)
+      self.version = version
+    end
+
+    def encode(encodable)
+      encoded = case encodable
+      when Hash then @versioned_coder_klass.encode(encodable.to_a.join(':'))
+      # when Hash then @versioned_coder_klass.encode(encodable.to_json.gsub(' ',''))
+      when String then @versioned_coder_klass.encode(encodable)
+      end
+      "#{encoded}v#{@version}"
+    end
+
+    def decode(decodable)
+      @versioned_coder_klass.decode(decodable)
+    end
+
+    def decode_to_hash(decodable)
+      HashWithIndifferentAccess[*@versioned_coder_klass.decode(decodable).split(':')]
+      # HashWithIndifferentAccess.new( ActiveSupport::JSON.decode( @versioned_coder_klass.decode(decodable) ) )
+    end
+
+    def version=(version)
+      begin
+        @versioned_coder_klass = "#{self::class}::VersionedCoder#{version}".constantize
+      rescue
+        raise(InvalidCoderVersion.new("coder VersionedCoder#{version} does not exist"))
+      end
+      @version = version
+    end
+
+
+    # returns something like ['astalavista', 898] from the string "astalavistav898"
+    def self.prepare_decodable_for_decoding(decodable)
+      if ( decodable = decodable.split(/^(.+)v([\d]+)$/) ).size == 3 # why 3 not 2 ?
+        [decodable.second, decodable.third.to_i]
+      else
+        raise InvalidVersionFormat.new("can't find version info in #{decodable}")
+      end
+    end
+
+    def self.encode(encodable)
+      new(@@version).encode(encodable)
+    end
+
+    def self.decode(decodable)
+      decodable, version = prepare_decodable_for_decoding(decodable)
+      new(version).decode(decodable)
+    end
+
+    def self.decode_to_hash(decodable)
+      decodable, version = prepare_decodable_for_decoding(decodable)
+      new(version).decode_to_hash(decodable)
+    end
+
+
+    # simple reverse for test stuff
+    if Rails.env.test?
+      class VersionedCoder1
+        def self.encode(string)
+          string.reverse
+        end
+
+        def self.decode(string)
+          string.reverse
+        end
+      end
+    end
+
+  end
+end
+
+# deprecated
+if Rails.env.test?
+  module DingDealer
+    class Guid
+
+      # simple hex encoding
+      class VersionedCoder2
+        def self.encode(string)
+          string.unpack('U'*string.length).collect {|c| c.to_s(16)}.to_s
+        end
+
+        def self.decode(string)
+          string.scan(/\w./).map{|x| Integer("0x#{x}").chr }.to_s
+        end
+      end
+
+    end
+  end
+end
+
+
+module DingDealer
+  class Guid
+
+    # simple modified base64 encoding, slightly modified to fit rfc3548
+    class VersionedCoder3
+      def self.encode(string)
+        Base64.encode64s(string).tr("+/","-_")
+      end
+
+      def self.decode(string)
+        Base64.decode64(string.tr("-_","+/"))
+      end
+    end
+
+  end
+end
