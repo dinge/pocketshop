13493b4c3606d20daa2322d22b8be8510b9969c0
Lars Gollnow <lg@mfind.de>
2009-03-08 18:46:30 +0100

    * removed xgen stuff

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,7 +1,5 @@
 class Tagging
   is_a_neo_relation :meta_info => true
 
-  # include Neo4j::RelationMixin
-  
   property :name
 end
--- lib/xgen_extensions.rb
+++
@@ -1,8 +0,0 @@
-# must be required in public/xgen_dispatch
-# like:
-# require 'xgen/rails'
-# require 'xgen_extensions'
-# Dispatcher.dispatch
-
-# require 'xgen_extensions/dispatcher_hack'
-# require 'xgen_extensions/mongo'
