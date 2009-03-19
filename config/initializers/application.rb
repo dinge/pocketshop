aed90ae53dcd1d8b0782965f1d72fa23a27ffcd0
Lars Gollnow <lg@mfind.de>
2009-03-19 21:55:53 +0100

    * require DingDealer::Struct

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -4,7 +4,10 @@ require 'my'
 require 'digest/sha1'
 
 # require 'ruby_extensions'
+require 'ding_dealer/struct'
 require 'ding_dealer/guid'
+require 'ding_dealer/rest'
+require 'ding_dealer/acl'
 
 Object.class_eval do
   include My
