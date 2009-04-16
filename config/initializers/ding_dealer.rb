7c5c20b8a3aaeebf936bc27b8be3b73ef8c1d597
Lars Gollnow <lg@mfind.de>
2009-04-16 18:10:18 +0200

    * some more DingDealer initialization refactorings

---
+++ config/initializers/ding_dealer.rb
@@ -0,0 +1,22 @@
+class Object
+  def dingsl_accessor(*attributes, &block)
+    DingDealer::Struct.new(*attributes, &block)
+  end
+end
+
+Object.class_eval { include DingDealer::My }
+
+unless defined?(ActiveRecord)
+  require 'ding_dealer/neo4j/active_record_stubs' 
+  ActiveRecord.require 'active_record/validations'
+end
+
+Object.class_eval do
+  include DingDealer::Neo4j::Node
+  include DingDealer::Neo4j::Relation
+end
+
+ActionController::Base.class_eval do
+  include DingDealer::Rest
+  include DingDealer::Acl
+end
