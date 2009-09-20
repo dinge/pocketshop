9e43a6f4bcb47d51439aa4e47b2bfb96b1825135
Lars Gollnow <lg@mfind.de>
2009-09-20 23:54:11 +0200

    * added some more tests

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -17,28 +17,38 @@ describe Word do
       Word.properties?(:name).should be_true
     end
 
-    it "every word should be unique in its language" do
+    it "every word should be unique in its language", ', by using Word.new_uniqe_from_language' do
       delete_all_nodes_from Word, Language
       english = Language.new(:code => 'en')
       deutsch = Language.new(:code => 'de')
 
       # restart_transaction
-
-
       Word.should have(:no).nodes
-      Word.new_uniqe_from_language(:name => 'soap', :language => english)
+      Word.new_uniqe_from_language(:name => 'angst', :language => english)
 
       restart_transaction
       Word.should have(1).nodes
 
-      first_uniqe_word = Word.new_uniqe_from_language(:name => 'kindergarten', :language => english)
+      first_english_kindergarten = Word.new_uniqe_from_language(:name => 'kindergarten', :language => english)
 
       restart_transaction
       Word.should have(2).nodes
 
-      second_uniqe_word = Word.new_uniqe_from_language(:name => 'kindergarten', :language => english)
+      second_english_kindergarten = Word.new_uniqe_from_language(:name => 'kindergarten', :language => english)
       Word.should have(2).nodes
-      first_uniqe_word.should == second_uniqe_word
+      first_english_kindergarten.should == second_english_kindergarten
+
+      restart_transaction
+
+      first_german_kindergarten = Word.new_uniqe_from_language(:name => 'kindergarten', :language => deutsch)
+      Word.should have(3).nodes
+      first_german_kindergarten.should_not == first_english_kindergarten
+      first_german_kindergarten.should_not == second_english_kindergarten
+
+      restart_transaction
+      second_german_kindergarten = Word.new_uniqe_from_language(:name => 'kindergarten', :language => deutsch)
+      Word.should have(3).nodes
+      first_german_kindergarten.should == second_german_kindergarten
     end
 
   end
