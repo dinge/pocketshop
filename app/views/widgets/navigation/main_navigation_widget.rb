815ba6d92f91313e151e0ad886f783cc14907935
Lars Gollnow <lg@mfind.de>
2010-01-01 18:31:25 +0100

    * added triples and phrases to main_nav

--- app/views/widgets/navigation/main_navigation_widget.rb
+++ app/views/widgets/navigation/main_navigation_widget.rb
@@ -3,6 +3,17 @@ class Views::Widgets::Navigation::MainNavigationWidget < Views::Widgets::Base
   def content
     control_list_container :id => :main_navigation, :class => :navigation do
       [
+
+        helpers.link_to('triples',
+          tools_phrase_maker_triples_path,
+          :class => dom_class_for_active_gizmo(:triples, controller.controller_name),
+          :accesskey => 't'),
+
+        helpers.link_to('phrases',
+          tools_phrase_maker_phrases_path,
+          :class => dom_class_for_active_gizmo(:phrases, controller.controller_name),
+          :accesskey => 't'),
+
         helpers.link_to('things',
           things_path,
           :class => dom_class_for_active_gizmo(:things, controller.controller_name),
