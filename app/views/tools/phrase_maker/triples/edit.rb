1b45d42b39cd6ca2a1e3351fe9e9ba58812c6cf3
Lars Gollnow <lg@mfind.de>
2010-01-03 22:32:13 +0100

    * stabilty fix

--- app/views/tools/phrase_maker/triples/edit.rb
+++ app/views/tools/phrase_maker/triples/edit.rb
@@ -10,26 +10,28 @@ class Views::Tools::PhraseMaker::Triples::Edit < Views::Layouts::Application
 
     ul :class => :tab_control do
       grammar_attributes.each do |ga|
-        phrase = current_object.phrase_as(ga)
-        li 'data-tab-id' => dom_id(phrase, 'tab') do
-          text '%s (%s)' % [phrase.name, phrase.triples.to_a.size]
+        if phrase = current_object.phrase_as(ga)
+          li 'data-tab-id' => dom_id(phrase, 'tab') do
+            text '%s (%s)' % [phrase.name, phrase.triples.to_a.size]
+          end
         end
       end
     end
 
     div :class => :tab_content do
       grammar_attributes.each do |ga|
-        phrase = current_object.phrase_as(ga)
-        dl :id => dom_id(phrase, :tab) do
-          grammar_attributes.each do |gs|
-            dt do
-              text link_to_gizmo(phrase).to_s << ' as %s (%s)' % [gs, phrase.triples_as(gs).to_a.size]
-            end
-            dd do
-              widget Views::Tools::PhraseMaker::Widgets::RelationshipsWidget.new(
-                :gizmo => phrase,
-                :method => gs,
-                :discard_headline => true )
+        if phrase = current_object.phrase_as(ga)
+          dl :id => dom_id(phrase, :tab) do
+            grammar_attributes.each do |gs|
+              dt do
+                text link_to_gizmo(phrase).to_s << ' as %s (%s)' % [gs, phrase.triples_as(gs).to_a.size]
+              end
+              dd do
+                widget Views::Tools::PhraseMaker::Widgets::RelationshipsWidget.new(
+                  :gizmo => phrase,
+                  :method => gs,
+                  :discard_headline => true )
+              end
             end
           end
         end
