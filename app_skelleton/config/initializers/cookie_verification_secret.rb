73b1b33c13cd75eaf2d3125020d5642af4e44702
Lars Gollnow <lg@mfind.de>
2010-03-23 18:45:14 +0100

    * added fresh app skelleton

---
+++ app_skelleton/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = '18a14da3406fae63de52dec74a011599720f015eda2198448c1cfb89b55ec9d989eec6417bc6c66ab507fe07b7602d46c380437e8da762a64d726f264cf28bab'
