0db7ef5083e49e752576c7b320fc106d467297ee
Lars Gollnow <lg@mfind.de>
2010-03-23 19:15:04 +0100

    * removed erector

--- vendor/plugins/erector/lib/erector/rails/rails_version.rb
+++
@@ -1,6 +0,0 @@
-module Erector
-  module Rails
-    RAILS_VERSION = "2.3.4"
-    RAILS_VERSION_TAG = "v2.3.4"
-  end
-end
--- vendor/plugins/erector/lib/erector/widgets.rb
+++
@@ -1,5 +0,0 @@
-require "erector/widgets/environment_badge"
-require "erector/widgets/field_table"
-require "erector/widgets/form"
-require "erector/widgets/page"
-require "erector/widgets/table"
